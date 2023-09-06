949508d86ddd86724c9792ec4b374241c056cad3
seville <amirrezasalimi0@gmail.com>
2023-09-06 14:06:31 +0330

    chore(zone): fix some bugs

---
+++ zone/frontend/src/shared/components/routes.tsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter, useRoutes } from "react-router-dom";
+import ManagerLayout from "./manager-layout";
+import Manager from "../../modules/manager";
+import Auth from "../../modules/auth";
+
+const Routes = () => {
+
+    return elements
+}
+export default Routes;
