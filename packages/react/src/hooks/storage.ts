c80d3de13664847fecbe938f2bbc8f950b0eb9b9
seville <amirrezasalimi0@gmail.com>
2023-09-07 10:28:10 +0330

    chore(react): remove old codes

--- packages/react/src/hooks/storage.ts
+++ packages/react/src/hooks/storage.ts
@@ -1,33 +1,16 @@
 import { JSONArray } from "immer-yjs"
 import useRoom from "./room"
 import { useSyncExternalStoreWithSelector } from 'use-sync-external-store/with-selector';
-import { useIsConnected } from "./connection";
-import { useRef } from "react";
 
-export function useStorage<State, Selection>(selector: (state: State) => Selection, defaults?: State) {
+export function useStorage<State, Selection>(selector: (state: State) => Selection) {
     type FakeState = State & JSONArray
-
     const { yImmerBinder } = useRoom();
-    const isConnected = useIsConnected();
     const selection = useSyncExternalStoreWithSelector(
         yImmerBinder.subscribe,
         yImmerBinder.get,
         yImmerBinder.get,
         selector,
     )
-    const defaultAlreadySet = useRef(false)
-
-    if (defaults && !defaultAlreadySet.current && isConnected) {
-        yImmerBinder.update(draft => {
-            Object.keys(defaults).forEach((key: any) => {
-                if (typeof draft[key] != 'undefined') return
-                // @ts-ignore
-                // draft[key] = defaults[key]
-                // alert(`set defaults ${key}`)
-            });
-            defaultAlreadySet.current = true;
-        })
-    }
     return [selection, yImmerBinder.update] as [
         Selection,
         (updater: (draft: FakeState) => void) => void
